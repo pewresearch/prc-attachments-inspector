c97b6bc4c77d2f9d28936a18a178db83fb4000e0
prcdevgitbot <webdev@pewresearch.org>
2026-07-21 14:55:07 +0000

    See https://github.com/pewresearch/prc-platform/commit/b18ceb7c8c0a631b919e50de2a0e1b2dc88cd93d from refs/tags/v1.12.0

--- prc-attachments-inspector.php
+++ prc-attachments-inspector.php
@@ -32,34 +32,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! defined( 'DEFAULT_TECHNICAL_CONTACT' ) ) {
-	define( 'DEFAULT_TECHNICAL_CONTACT', 'webdev@pewresearch.org' );
-}
-
 define( 'PRC_ATTACHMENTS_INSPECTOR_FILE', __FILE__ );
 define( 'PRC_ATTACHMENTS_INSPECTOR_DIR', __DIR__ );
 define( 'PRC_ATTACHMENTS_INSPECTOR_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-activator.php
- */
-function activate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-activator.php';
-	Plugin_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-deactivator.php
- */
-function deactivate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-deactivator.php';
-	Plugin_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, '\PRC\Platform\Attachments_Inspector\activate' );
-register_deactivation_hook( __FILE__, '\PRC\Platform\Attachments_Inspector\deactivate' );
 
 /**
  * Helper utilities
